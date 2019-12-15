3e07bc5c6faef53a16abb71bbfdbbc87c0f6e7ac
Talal Buali <tb00630@surrey.ac.uk>
2019-12-15 21:48:38 +0000

    Added contact mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default to: "admin1@gmail.com", from: 'from@example.com'
   layout 'mailer'
 end
--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -25,7 +25,7 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
 
   test "should post request contact" do
     post request_contact_url, params:
-    {name: "Mathew", email: "mathew@me.com", telephone: "1234567890", message: "Hello"}
+    {name: "Matthew", email: "matthew@me.com", telephone: "1234567890", message: "Hello"}
     assert_response :redirect
       assert_nil flash[:alert]
       assert_not_empty flash[:notice]
