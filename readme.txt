f2affd01a484df7839f02344172a36abb77ddea1
Talal Buali <tb00630@surrey.ac.uk>
2019-12-16 08:07:51 +0000

    Last edits

---
+++ readme.txt
@@ -0,0 +1,9 @@
+The link to my github is https://github.com/Talzo/com2025cw
+
+To login as an admin:
+	email: admin1@gmail.com
+	password: admin1
+
+To login as a user:
+	email: user1@gmail.com
+	password: user11
--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -11,13 +11,4 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
   test "the truth" do
     assert true
   end
-
-# Create new comment
-  test "should create comment" do
-    assert_difference('Comment.count') do
-      post :create, comment: { body: @comment.body }, post_id: @post.id
-    end
-    assert_redirected_to post_path(assigns(:post))
-  end
-
 end
